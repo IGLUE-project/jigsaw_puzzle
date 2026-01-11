4f7ed8e23ccfc50e7544787d5ae08dcdb03c373d
unknown <agordillos@gmail.com>
2026-01-11 20:13:52 +0100

    Update image links

--- config-example.js
+++ config-example.js
@@ -3,12 +3,10 @@
 export let ESCAPP_APP_SETTINGS = {
   //Settings that can be specified by the authors
   //backgroundImg: "", //background can be "NONE" or a URL.
-
-  image1:"https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D",
-  image2:"https://img.freepik.com/free-photo/courage-man-jump-through-gap-hill-business-concept-idea_1323-262.jpg?semt=ais_hybrid&w=740&q=80",
+  image1:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Laguna_69.png/960px-Laguna_69.png",
+  image2:"https://upload.wikimedia.org/wikipedia/commons/3/3d/A_Landscape_with_Travellers_and_Cattle_Crossing_a_Bridge_by_a_Waterfall_.PNG",
   rows: 3,
   cols: 3,
-
 
   //Settings that will be automatically specified by the Escapp server
   locale: "es",
